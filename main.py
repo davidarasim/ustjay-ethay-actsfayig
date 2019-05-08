95345dbdad78c12bf2645cade07e6d9f33ecb47d
Dave Arasim <davidarasim@yahoo.com>
2019-05-07 19:24:46 -0700

    Lesson 5 submittal

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, send_file, Response
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET_KEY').encode()
+# app.secret_key = os.environ.get('SECRET_KEY').encode()
 
 def get_fact():
     response = requests.get("http://unkno.com")
